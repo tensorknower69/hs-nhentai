716e776571bd50e6013efbb1fcb477781b329d15
tensorknower69 <62006583+tensorknower69@users.noreply.github.com>
2020-10-10 02:00:31 +0800

    use tasty

--- test/Test.hs
+++ test/Test.hs
@@ -2,21 +2,28 @@
 {-# LANGUAGE TypeApplications #-}
 
 import Data.Aeson
+import Data.Either
 import Data.NHentai.API
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import Refined
+import Test.Tasty
+import Test.Tasty.HUnit
 import Text.URI (render)
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
 
-main :: IO ()
-main = do
-	url <- refineThrow 41852 >>= mkGalleryApiUrl
+tests :: TestTree
+tests = testGroup "testing Data.NHentai.API" [testGalleryApi 41753]
+
+testGalleryApi :: Int -> TestTree
+testGalleryApi gid = testCase ("download " <> show gid) $ do
+	url <- refineThrow gid >>= mkGalleryApiUrl
 	mgr <- newManager tlsManagerSettings
 	req <- parseRequest (T.unpack $ render url)
 	rep <- httpLbs req mgr 
-	print rep
-	let a = eitherDecode @APIGallery (responseBody rep)
-	print a
+	let result = eitherDecode @APIGallery (responseBody rep)
+	assertBool "can decode json" (isRight result)
 
+main :: IO ()
+main = defaultMain tests
